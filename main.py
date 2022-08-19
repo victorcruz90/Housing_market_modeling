c2ab3e3f75c329c1806b97b072b306e9a181b9e9
victorcruz90 <victorcruzdefaria@gmail.com>
2022-08-19 22:35:04 +1000

    fix bug on input and add .gitignore

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from scraper import Scraper
-import csv
 import time
 import random
 
 def main():   
-    suburb_name = str(input("Suburb: ")).lower
-    state = str(input("State (e.g VIC, NSW...): ")).capitalize
+    suburb_name = str(input("Suburb: "))
+    state = str(input("State (e.g vic, nsw...): "))
     suburb_post = str(input("Enter postcode (XXXX): "))
     price = str(input("Price range (as XXXX-XXXXXX): "))
-
-
+   
     page = 1
     while True:
         try:
             PAGE_URL = f'https://www.domain.com.au/sold-listings/{suburb_name}-{state}-{suburb_post}/?price={price}&excludepricewithheld=1&page={page}'
-            scraped_data = Scraper(PAGE_URL).get_details().to_csv('/data/housing_data.csv', mode='a',index=False, date_format='sold_date')
+            scraped_data = Scraper(PAGE_URL).get_details()
             if scraped_data.empty:
                 raise Exception
             else:
@@ -22,6 +20,7 @@ def main():
         except Exception:
             print("Page Empty")
             break
+        scraped_data.to_csv('/data/housing_data.csv', mode='a',index=False, date_format='sold_date')
         timeout = random.randrange(1,15)
         print(timeout)
         time.sleep(timeout)
