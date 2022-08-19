f914df731444ba0d8a5abff438929d8a3b439805
victorcruz90 <victorcruzdefaria@gmail.com>
2022-08-19 22:02:00 +1000

    Introduced main fuction

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Housing_market_modeling
 Scrape data and analyse it for ML predictions
+
+## Motivation
+
+As the Australian house market rises, the affoarbility of house becomes a hot topic between young people, which are trying to get into the market. My motivation for this project was develop some of my data and coding skills by gathering and analysing housing prices across Victoria and potentially other states. 
+
+## Project
+
+The project initially consist of scraping house prices from sold properties in the last 10 years from the website "Domain real state". The data will have the address, state, prince, type of housing, and date sold.
+
+## Issue1
+
+Initially, the project was meant to create thousand of URL based on a list of postocode (postcode.csv). However, it would take too long. Therefore, the data will scrape based on user input. The input will be: suburb name, suburb postcode, state. 
+
+There will be automation for the price range. Restricting the price range helps to scrape more results since the website only makes 50 pages available for each search regardless of how many results it will have.
--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-import pandas as pd
+from scraper import Scraper
+import csv
+import time
+import random
 
-# BASE_URL = f'https://www.domain.com.au/sold-listings/?postcode={x}&price=0-5000000&excludepricewithheld=1'
-# PAGE_URL = f'https://www.domain.com.au/sold-listings/?postcode={x}&price=0-5000000&excludepricewithheld=1&page={y}'
-suburb_post = pd.read_csv('postcode.csv')
-states = ['VIC', 'NSW', 'SA', 'WA', 'TAS', 'QLD']
+def main():   
+    suburb_name = str(input("Suburb: ")).lower
+    state = str(input("State (e.g VIC, NSW...): ")).capitalize
+    suburb_post = str(input("Enter postcode (XXXX): "))
+    price = str(input("Price range (as XXXX-XXXXXX): "))
 
 
-    # for x in suburb_post[state]:
-
+    page = 1
+    while True:
+        try:
+            PAGE_URL = f'https://www.domain.com.au/sold-listings/{suburb_name}-{state}-{suburb_post}/?price={price}&excludepricewithheld=1&page={page}'
+            scraped_data = Scraper(PAGE_URL).get_details().to_csv('/data/housing_data.csv', mode='a',index=False, date_format='sold_date')
+            if scraped_data.empty:
+                raise Exception
+            else:
+                print(f'Page {page}: Done')
+        except Exception:
+            print("Page Empty")
+            break
+        timeout = random.randrange(1,15)
+        print(timeout)
+        time.sleep(timeout)
+        page += 1
+    
+    
+    
         
-
-    # suburb_postcode = pd.read_csv('postcode.csv')
-    # for x in suburb_postcode[state]:
-    #     for y in range(1,number_pages+1):
-            
-            
-
 if __name__ == '__main__':
-    pass
+    main()
